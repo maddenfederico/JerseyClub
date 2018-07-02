cf818e08ed19064e351fb997bc76c0e65e55f07e
Federico Madden <maddenfederico@gmail.com>
2018-07-02 12:33:29 -0700

    Initial commit

---
+++ Generator.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator
+{
+    private List<String>[] nouns;
+    private List<String>[] adjectives;
+
+    public Generator(String adjFile, String nounFile)
+    {
+        
+    }
+}
